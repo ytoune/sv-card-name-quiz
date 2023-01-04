b2434bb0ae0b9f68c0482f3f0de991e32d3d1b03
ytoune <ytoune@users.noreply.github.com>
2023-01-04 18:21:40 +0900

    fix

--- scripts/gen-cards.ts
+++ scripts/gen-cards.ts
@@ -2,17 +2,21 @@ import { mkdir, writeFile } from 'fs/promises'
 import { join } from 'path'
 
 import info from '../data/cards'
+
+import type { Card } from '~/cards'
 ;(async () => {
-	const cards = info.data.cards.map(c => ({
-		id: c.card_id,
-		cardSet: c.card_set_id,
-		name: c.card_name,
-		type: c.char_type,
-		tribeName: c.tribe_name,
-		clan: c.clan,
-		rarity: c.rarity,
-		cv: c.cv,
-	}))
+	const cards: Card[] = info.data.cards.map(
+		(c): Card => ({
+			id: c.card_id,
+			cardSet: c.card_set_id,
+			name: c.card_name,
+			type: c.char_type,
+			tribeName: c.tribe_name,
+			clan: c.clan,
+			rarity: c.rarity,
+			cv: c.cv,
+		}),
+	)
 	await mkdir(join(__dirname, '../src/gen'), { recursive: true })
 	await writeFile(
 		join(__dirname, '../src/gen/cards.ts'),
