0b1d492b89d53dc833dc52649a5cec5066f2fddd
ytoune <ytoune@users.noreply.github.com>
2023-11-16 12:35:31 +0900

    fix

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,4 +1,5 @@
-import { build } from 'esbuild'
+import type { BuildOptions } from 'esbuild'
+import { build, context } from 'esbuild'
 import { cp } from 'node:fs/promises'
 import { join } from 'node:path'
 
@@ -7,7 +8,7 @@ const isDev = 'development' === process.env.NODE_ENV
 ;(async () => {
 	const root = join(__dirname, '..')
 	await cp(join(root, 'statics'), join(root, 'dist'), { recursive: true })
-	await build({
+	const config: BuildOptions = {
 		entryPoints: ['src/index.tsx'],
 		bundle: true,
 		outdir: 'dist',
@@ -19,8 +20,13 @@ const isDev = 'development' === process.env.NODE_ENV
 		inject: ['src/_import-preact.ts'],
 		jsxFactory: 'h',
 		jsxFragment: 'Fragment',
-		watch: isDev,
-	})
+	}
+	if (isDev) {
+		const ctx = await context({ ...config })
+		await ctx.watch()
+	} else {
+		await build({ ...config })
+	}
 })().catch(x => {
 	console.error(x)
 	process.exit(1)
